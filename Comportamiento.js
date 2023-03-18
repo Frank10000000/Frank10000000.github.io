9481bd9fe375d9d43fadaf9cc438e52c3a7b325d
Frank10000000 <86135552+Frank10000000@users.noreply.github.com>
2023-03-18 06:32:49 -0300

    Update Comportamiento.js

--- Comportamiento.js
+++ Comportamiento.js
@@ -1,7 +1,5 @@
 
-
-
-document.getElementById("contacto").onmouseover = function (){document.getElementById("informacion").innerHTML = "Telefono (3635145874) - Correo electronico (elLadoOscuro32@yahoo.com.ar)"}
-document.getElementById("direccion").onmouseover = function (){document.getElementById("informacion").innerHTML = "Via Lactea/Sistema Solar/Marte - En en el cráter 310"}
-document.getElementById("conosimiento").onmouseover = function (){document.getElementById("informacion").innerHTML = "Ganaderia, Construcción, Maquinaria pesada y Espadachin"}
-document.body.onclick = function (){document.getElementById("informacion").innerHTML = "Años en la soledad y con solo videojuegos para dibertirme, e decidido aprender a programar y asi hacerme mis propios juegos"}
+document.getElementById("contacto").onclick = function (){document.getElementById("informacion").innerHTML = "Telefono (3635145874) - Correo electronico (elLadoOscuro32@yahoo.com.ar)"}
+document.getElementById("direccion").onclick = function (){document.getElementById("informacion").innerHTML = "Via Lactea/Sistema Solar/Marte - En en el cráter 310"}
+document.getElementById("conosimiento").onclick = function (){document.getElementById("informacion").innerHTML = "Ganaderia, Construcción, Maquinaria pesada y Espadachin"}
+document.getElementById("informacion").onmouseover = function (){document.getElementById("informacion").innerHTML = "Años en la soledad y con solo videojuegos para dibertirme, e decidido aprender a programar y asi hacerme mis propios juegos"}
