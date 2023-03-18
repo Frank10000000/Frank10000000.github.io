f71d1170db169dbd6404cf57b00e1c3dea0a5d79
Frank10000000 <86135552+Frank10000000@users.noreply.github.com>
2023-03-18 01:33:23 -0300

    Add files via upload

---
+++ Comportamiento.js
@@ -0,0 +1,7 @@
+
+
+
+document.getElementById("contacto").onmouseover = function (){document.getElementById("informacion").innerHTML = "Telefono (3635145874) - Correo electronico (elLadoOscuro32@yahoo.com.ar)"}
+document.getElementById("direccion").onmouseover = function (){document.getElementById("informacion").innerHTML = "Via Lactea/Sistema Solar/Marte - En en el cráter 310"}
+document.getElementById("conosimiento").onmouseover = function (){document.getElementById("informacion").innerHTML = "Ganaderia, Construcción, Maquinaria pesada y Espadachin"}
+document.body.onclick = function (){document.getElementById("informacion").innerHTML = "Años en la soledad y con solo videojuegos para dibertirme, e decidido aprender a programar y asi hacerme mis propios juegos"}
